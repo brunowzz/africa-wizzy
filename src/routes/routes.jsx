2203299a71b3ca3f3815c8abef36c5a5dd4bcddc
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-03-17 18:55:37 -0300

    feat: add default layout

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 
+import DefaultLayout from "../layout/DefaultLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import TourPackage from "../pages/TourPackage";
@@ -12,12 +13,12 @@ import ContactUs from "../pages/ContactUs";
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
-    <>
+    <Route element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/tour-package" element={<TourPackage />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/contact-us" element={<ContactUs />} />
-    </>
+    </Route>
   )
 );
