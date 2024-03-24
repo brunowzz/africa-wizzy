1e2a98f6a1fdd9796d45e85dc0c02ca7aec10496
Pamela Raiane <pamelaprpn@gmail.com>
2024-03-24 13:36:41 -0300

    feat: add logic icons colors

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -2,20 +2,20 @@ import * as S from './style'
 
 import CustomIcon from '../CustomIcon'
 
-export default function SocialMedia (dark){
+export default function SocialMedia (dark, arc, size){
     return(
         <S.ContainerSocialMedia>
-            <S.ButtonSocialMedia>
-            <CustomIcon icon={'facebook'} size={20} color={dark ? '#fff' : '#304F47'} />
+            <S.ButtonSocialMedia arc={arc}>
+            <CustomIcon icon={'facebook'} size={size ? 24 : 15} color={dark ? '#fff' : '#304F47'} />
+            </S.ButtonSocialMedia >
+            <S.ButtonSocialMedia arc={arc}>
+            <CustomIcon icon={'twitter'} size={size ? 24 : 15} color={dark ? '#fff' : '#304F47'}/>
             </S.ButtonSocialMedia>
-            <S.ButtonSocialMedia>
-            <CustomIcon icon={'twitter'} size={20} color={dark ? '#fff' : '#304F47'}/>
+            <S.ButtonSocialMedia arc={arc}>
+            <CustomIcon icon={'linkedin'} size={size ? 24 : 15} color={dark ? '#fff' : '#304F47'}/>
             </S.ButtonSocialMedia>
-            <S.ButtonSocialMedia>
-            <CustomIcon icon={'linkedin'} size={20} color={dark ? '#fff' : '#304F47'}/>
-            </S.ButtonSocialMedia>
-            <S.ButtonSocialMedia>
-            <CustomIcon icon={'instagram'} size={20} color={dark ? '#fff' : '#304F47'}/>
+            <S.ButtonSocialMedia arc={arc}>
+            <CustomIcon icon={'instagram'} size={size ? 24 : 15} color={dark ? '#fff' : '#304F47'}/>
             </S.ButtonSocialMedia>
         </S.ContainerSocialMedia>
     )
--- src/components/SocialMedia/style.js
+++ src/components/SocialMedia/style.js
@@ -3,12 +3,15 @@ import styled from "styled-components";
 export const ContainerSocialMedia = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
     gap: 10px;
 `
 
 export const ButtonSocialMedia = styled.button`
     background: transparent;
     border-radius: 50%;
-    border: 1px solid;
-    padding: .5em;
+    border: 2px solid ${props => props.arc ? '#fff' : '#304F47'};
+    padding: .25em .5em  ;
+    
 `
+
