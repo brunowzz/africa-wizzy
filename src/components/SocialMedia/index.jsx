85d7aab580fad6a02f49c9a508bce253f127901c
Pamela Raiane <pamelaprpn@gmail.com>
2024-03-26 10:47:37 -0300

    fix: remove props

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -2,19 +2,19 @@ import * as S from './style'
 
 import CustomIcon from '../CustomIcon'
 
-export default function SocialMedia (variant, custom){
+export default function SocialMedia (custom){
     return(
-        <S.ContainerSocialMedia variant={variant}>
-            <S.ButtonSocialMedia variant={variant} >
+        <S.ContainerSocialMedia custom={custom}>
+            <S.ButtonSocialMedia custom={custom} >
                 <CustomIcon icon={'facebook'} size={custom ? 10 : 22} color={custom ? '#fff' : '#304F47'} />
             </S.ButtonSocialMedia >
-            <S.ButtonSocialMedia variant={variant}>
+            <S.ButtonSocialMedia custom={custom}>
                 <CustomIcon icon={'twitter'} size={custom ? 10 : 22} color={custom ? '#fff' : '#304F47'} />
             </S.ButtonSocialMedia>
-            <S.ButtonSocialMedia variant={variant}>
+            <S.ButtonSocialMedia custom={custom}>
                 <CustomIcon icon={'linkedin'} size={custom ? 10 : 22} color={custom ? '#fff' : '#304F47'} />
             </S.ButtonSocialMedia>
-            <S.ButtonSocialMedia variant={variant}>
+            <S.ButtonSocialMedia custom={custom}>
                 <CustomIcon icon={'instagram'} size={custom ? 10 : 22} color={custom ? '#fff' : '#304F47'} />
             </S.ButtonSocialMedia>
         </S.ContainerSocialMedia>
--- src/components/SocialMedia/style.js
+++ src/components/SocialMedia/style.js
@@ -4,15 +4,15 @@ export const ContainerSocialMedia = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    gap: ${props => props.variant ? '0.5rem' : '0.9rem'};
+    gap: ${props => props.custom ? '0.5rem' : '0.9rem'};
 `
 
 export const ButtonSocialMedia = styled.button`
     background: transparent;
     border-radius: 50%;
-    border: 2px solid ${props => props.variant ? '#fff' : '#304F47'};
+    border: 2px solid ${props => props.custom ? '#fff' : '#304F47'};
     padding: auto ;
-    width: ${props => props.variant ? '1.5rem' : '3.1rem'};
-    height: ${props => props.variant ? '1.5rem' : '3.1rem'};  
+    width: ${props => props.custom ? '1.5rem' : '3.1rem'};
+    height: ${props => props.custom ? '1.5rem' : '3.1rem'};  
 `
 
